ee98d2e172fc3491b923bc0db53324de35e14ceb
carloscoyac <carlos56.rgd@gmail.com>
2021-07-30 09:36:46 -0600

    Update forms.js

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -16,9 +16,9 @@ function sendData() {
       encodeURIComponent(form.querySelector("[name='name']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("send_to") +
+    encodeURIComponent("project") +
       "=" +
-      encodeURIComponent(form.querySelector("[name='send_to']").value)
+      encodeURIComponent(form.querySelector("[name='project']").value)
   );
   urlEncodedDataPairs.push(
     encodeURIComponent("email") +
@@ -26,49 +26,12 @@ function sendData() {
       encodeURIComponent(form.querySelector("[name='email']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("phone") +
+    encodeURIComponent("message") +
       "=" +
-      encodeURIComponent(form.querySelector("[name='phone']").value)
+      encodeURIComponent(form.querySelector("[name='message']").value)
   );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("date") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='date']").value)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("qty") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='qty']").value)
-  );
-
-  // radio buttons
-  let radio = document.getElementsByName("ufotype");
-  for (var i = 0, length = radio.length; i < length; i++) {
-    if (radio[i].checked) {
-      urlEncodedDataPairs.push(
-        encodeURIComponent("ufotype") + "=" + encodeURIComponent(radio[i].value)
-      );
-    }
-  }
-
-  // dropdown menu
-  var dropdown = form.querySelector("[name='abtype']");
-  urlEncodedDataPairs.push(
-    encodeURIComponent("abtype") +
-      "=" +
-      encodeURIComponent(dropdown.options[dropdown.selectedIndex].text)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("comments") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='comments']").value)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("subscribe") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='subscribe']").checked)
-  );
-
+  
+ 
   // Combine the pairs into a single string and replace all %-encoded spaces to
   // the '+' character; matches the behaviour of browser form submissions.
   urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
